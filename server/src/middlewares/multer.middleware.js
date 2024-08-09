1c01a2e953afb7546c367380fa1ac78c957ea536
AkshatKumarSinha9327 <kumarsinhaakshat8@gmail.com>
2024-08-09 17:21:12 +0530

    vercel trouble

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -3,7 +3,7 @@ import multer from "multer";
 const storage = multer.diskStorage({
     // file --> if file is coming cb --> call back
     destination: function (req, file, cb) {
-        cb(null, "/temp");
+        cb(null, "./public/temp");
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
